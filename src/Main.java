c56378e515c3d2fa941bee5204d788881f2c8715
yanastyas <yanastyas28@mail.ru>
2023-02-02 22:32:22 +1000

    HashSet

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    protected static String text() {
+        return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    }
+
+    public static void main(String[] args) {
+        String text = text();
+        WordsChecker wordChecker = new WordsChecker(text);
+        System.out.println(wordChecker.hasWord("consectetur"));
+        System.out.println(wordChecker.hasWord("mother"));
+    }
+}
---
+++ src/WordsChecker.java
@@ -0,0 +1,21 @@
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class WordsChecker {
+
+    public String hasWord;
+    protected String text;
+    Set<String> set = new HashSet<>();
+
+    public WordsChecker(String text) {
+        this.text = text;
+        String[] word = text.split("\\P{IsAlphabetic}+");
+        Collections.addAll(set, word);
+    }
+
+    public boolean hasWord(String word) {
+        return text.contains(word);
+    }
+}
